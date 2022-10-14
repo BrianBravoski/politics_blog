aba9fb84a64ea66ee19069d86d584bf7ae2272dc
BrianBravoski <brianbravoski28@gmail.com>
2022-10-14 12:17:52 +0300

    post details component

--- services/index.js
+++ services/index.js
@@ -96,24 +96,25 @@ export const getRecentPosts = async () => {
 }
 
 
-export const getSimilarPosts = async () => {
+export const getSimilarPosts = async (categories, slug) => {
   const query = gql`
     query getPostDetails($slug: String! , $categories: [String!]){
         posts(
             where: {slug_not: $slug, AND: {categories_some: {slug_in: $categories}}}
             last: 3
         )
-    }{
+    {
         title
-        featuredImage{
+        featuredImage{ 
             url
         }
         createdAt
         slug
     }
+  }
     `;
 
-  const result = await request(graphqlAPI, query);
+  const result = await request(graphqlAPI, query ,{categories, slug});
 
   return result.posts;
 }
