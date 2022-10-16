5cab1e100102e69270f0b172e59a27153777546f
BrianBravoski <brianbravoski28@gmail.com>
2022-10-16 23:51:17 +0300

    comment form

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { GraphQLClient,gql } from "graphql-request"
+import { GraphQLClient,gql } from "graphql"
 
 
 const graphqlAPI = process.env.NEXT_PUBLIC_POLITICS_ENDPOINT;
@@ -20,13 +20,21 @@ export default async function asynchandler(req, res) {
     }
     `
 
-    const result = await graphQLClient.request(query, {
-      name: req.body.name,
-      email: req.body.email,
-      comment: req.body.comment,
-      slug: req.body.slug,
-    });
+    try {
+      const result = await graphQLClient.request(query, {
+        name: req.body.name,
+        email: req.body.email,
+        comment: req.body.comment,
+        slug: req.body.slug,
+      });
+  
+      return res.status(200).send(result);
+    } catch (error) {
+      console.log(error)
+      return res.status(500).send(error);
+      
+    }
 
-    return res.status(200).send(result);
+    
 
   }
